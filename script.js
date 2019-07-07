920a4390fbf6031f5492b24e447adbd950f1e9fa
Quique <enrique.jmz37@gmail.com>
2019-07-06 23:15:04 -0500

    3rd commit

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 /*************************
- *Variables and data types 
+ *letiables and data types 
  */
 /*
-var firstName = 'John';
+let firstName = 'John';
 console.log(firstName);
 
-var lastName = 'Smith';
-var age = 28;
+let lastName = 'Smith';
+let age = 28;
 
-var fullAge = true;
+let fullAge = true;
 console.log(fullAge);
 
-var job;
+let job;
 console.log(job);
 
 job = 'Teacher';
 console.log(job);
 
-// Variable naming rules
-var _3years = 3;
-var $4years = 4;
-var johnMark = 'John and Mark';
+// letiable naming rules
+let _3years = 3;
+let $4years = 4;
+let johnMark = 'John and Mark';
 
 /*************************
- * Variable mutation and type coercion
+ * letiable mutation and type coercion
  */
 
 /*
- var firstName = 'John';
-var age = 28;
+ let firstName = 'John';
+let age = 28;
 
 //Type coercion
 console.log(firstName + ' ' + age);
 
-var job, isMarried;
+let job, isMarried;
 job = 'teacher';
 isMarried = false;
 
 console.log(firstName + ' is a ' + age + ' year old ' 
 + job  + '. Is he married? ' + isMarried);
 
-// Variable mutation
+// letiable mutation
 age = 'twenty eight';
 job = 'driver';
 
 alert(firstName + ' is a ' + age + ' year old ' 
 + job  + '. Is he married? ' + isMarried)
 
-var lastName = prompt('What is his last Name?');
+let lastName = prompt('What is his last Name?');
 console.log(firstName + ' ' + lastName);
 
 /*******************************
@@ -55,7 +55,7 @@ console.log(firstName + ' ' + lastName);
  */
 
  /*
-var year, yearJohn, yearMark;
+let year, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
 ageMark = 33;
@@ -71,14 +71,14 @@ console.log(now * 2);
 console.log(now / 10);
 
 // Logical operators
-var johnOlder = ageJohn < ageMark;
+let johnOlder = ageJohn < ageMark;
 console.log(johnOlder);
 
 // typeof operator
 console.log(typeof johnOlder);
 console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
-var x;
+let x;
 console.log(typeof x);
 
 /*******************************
@@ -86,23 +86,23 @@ console.log(typeof x);
  */
 
  /*
-var now = 2018;
-var yearJohn = 1989;
-var fullAge = 18;
+let now = 2018;
+let yearJohn = 1989;
+let fullAge = 18;
 
 // Multiple Operators
-var isFullAge = now -yearJohn >= fullAge; //true
+let isFullAge = now -yearJohn >= fullAge; //true
 console.log(isFullAge);
 
 //Grouping
-var ageJohn = now - yearJohn;
-var ageMark = 35;
-var average = (ageJohn + ageMark) / 2;
+let ageJohn = now - yearJohn;
+let ageMark = 35;
+let average = (ageJohn + ageMark) / 2;
 console.log(average);
 
 // Multiple assignments
 
-var x, y;
+let x, y;
 x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 
 // 32 -6 // 26
 console.log(x, y);
@@ -134,16 +134,16 @@ console.log(x);
 Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the 
 formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
 
-1. Store Mark's and John's mass and height in variables
+1. Store Mark's and John's mass and height in letiables
 2. Calculate both their BMIs
-3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
-4. Print a string to the console containing the variable from step 3. 
+3. Create a boolean letiable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the letiable from step 3. 
 (Something like "Is Mark's BMI higher than John's? true"). 
 
 GOOD LUCK 😀
 */
 /*
-var markMass, markHeight, johnMass, johnHeight, bmiMark, bmiJohn, highestBMI;
+let markMass, markHeight, johnMass, johnHeight, bmiMark, bmiJohn, highestBMI;
 
 
 markHeight = 1.80;
@@ -163,8 +163,9 @@ console.log('Is Mark\'s BMI higher than John\'s? ' + highestBMI)
  * If / else statements
  */
 
-var firstName = 'John';
-var civilStatus = 'single';
+ /*
+let firstName = 'John';
+let civilStatus = 'single';
 
 if (civilStatus === 'married') {
     console.log(firstName + ' is married!');
@@ -172,14 +173,14 @@ if (civilStatus === 'married') {
     console.log(firstName + ' will hopefully marry soon :)');
 }
 
-var isMarried = true;
+let isMarried = true;
 if (isMarried) {
     console.log(firstName + ' is married!');
 } else {
     console.log(firstName + ' will hopefully marry soon :)');
 }
 
-var markMass, markHeight, johnMass, johnHeight, bmiMark, bmiJohn, highestBMI;
+let markMass, markHeight, johnMass, johnHeight, bmiMark, bmiJohn, highestBMI;
 
 markHeight = 1.80; // meter
 markMass = 75; // kg
@@ -196,8 +197,210 @@ if (bmiMark > bmiJohn) {
     console.log('John\'s BMI is higher than Mark\'s.');
 }
 
+*/
+
 //highestBMI = bmiMark > bmiJohn;
 //console.log(bmiMark, bmiJohn);
 //console.log('Is Mark\'s BMI higher than John\'s? ' + highestBMI) 
+
+
+/******************************
+ * Boolean Logic
+ */
+
+ /*
+let firstName = 'John';
+let age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+}  else if (age >= 13 && age < 20 ) {  // Between 13 and 20 === age >= 13
+    // & age < 20
+    console.log(firstName + ' is a teenager.');
+}  else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
+}   else {
+    console.log(firstName + ' is a man.');
+}
+
+/*********************************
+ * The Ternary Operator and Switch Statements 
+ */
+
+/// Else if statement all in one line
+
+/*
+let firstName = 'John';
+let age = 18;
+
+// Ternary Operator
+age >= 18 ? console.log(firstName + ' drinks beer.')
+ : console.log(firstName + ' drinks juice.');
+
+let drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+if (age >= 18) {  //// This is the same as the ternary operator but with if and else, ternary is better for easy operations
+    // If, else, and else if are better for more complex operations 
+    let drink = 'beer';
+} else {
+    let drink = 'juice';
+}
+
+// Switch statement
+let job = 'instructor';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+age = 56;
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
+*/
+
+/***************************************
+ * Truthy and Falsy values and equality operators
+ */
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+/*
+let height;
+height = '23';
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
+}
+
+// Equality operators
+if (height == 23) {
+    console.log('The == operator does type coercion!');
+}
+
+/**************************************
+ * CODING CHALLENGE 2
+ */
+
+ /*
+ John and Mike both play basketball in different teams. In the
+ latest 3 games, John's team scored 89, 120 and 103 points, while
+ Mike's team scored 116, 94 and 123 points.
+
+ 1. Calculate the average score for each team.
+ 2. Decide which teams wins in average (highest average score), 
+ and print the winner to the console. Also include the average
+ score in the output.
+ 3. Then change the scores to show different winners. Don't forget
+ to take into account there might be a draw (the same average score).
+
+ 4. EXTRA: Mary also plays basketball, and her team scored 97,
+ 134 and 105 points. Like before, log the average winner to the
+ console. HINT: you will need the && operator to take the decision.
+ If you can't solve this one, just watch the solution, it's no problem
+ :)
+ 5. Like before, change the scores to generate different winners,
+ keeping in mind there might be draws.
+
+ GOOD LUCK 
+ */
+
+/*
+ 1.
+
+teamJohn = (89 + 120 + 103) / 3;
+console.log('John\'\s team made on average ' + teamJohn + ' points.' );
+teamMike = (116 + 94 + 123) / 3;
+console.log('Mike\'\s team made on average ' + teamMike + ' points.') ;
+teamMary = (97 + 134 + 105) / 3;
+console.log('Mary\'\s team made on average ' + teamMary + ' points.');
+
+2.
+
+teamJohn = 101;
+teamMike = 102;
+teamMary = 102;
+
+if (teamJohn > teamMike && teamJohn > teamMary) {
+    console.log('John\'\s team wins with an average score of ' + teamJohn + ' points');
+} else if (teamMike > teamMary && teamMike > teamJohn) {
+    console.log('Mike\'\s team wins with an average score of ' + teamMike + ' points');
+} else if (teamMary > teamJohn && teamMary > teamMike) {
+    console.log('Mary\'\s team wins with an average score of ' + teamMary + ' points');
+} else if (teamJohn === teamMike && teamJohn === teamMary) {
+    console.log('The 3 teams are tied up with an average score of ' + teamJohn + ' points');
+} else if (teamJohn === teamMike) {
+    console.log('John\'\s and Mike\'\s teams are tied up with an average score of ' + teamJohn + ' points');
+} else if (teamJohn === teamMary) {
+    console.log('John\'\s and Mary\'\s teams are tied up with an average score of ' + teamJohn + ' points');
+} else {
+    console.log('Mike\'\s and Mary\'\s teams are tied up with an average score of ' + teamMike + ' points');
+}
+
+/*******************************************************************************
+ * Functions
+ */
+
+ function calculateAge(birthYear) {
+    return 2018 - birthYear;
+ }
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired.');
+    }
+}
+    
+    yearsUntilRetirement(1990, 'John');
+    yearsUntilRetirement(1948, 'Mike');
+    yearsUntilRetirement(1969, 'Jane');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
